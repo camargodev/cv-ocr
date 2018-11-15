e5dafdf4aa1da55e29e9a91193a39c770af89723
João Vitor de Camargo <camargo.dev@gmail.com>
2018-11-15 10:24:53 -0200

    Enhancement on code organization

--- textprocessor.py
+++ textprocessor.py
@@ -1,5 +1,9 @@
 import numpy as np
 import cv2 as cv
+import os
+
+inputFolder   = "input"
+outputFolder  = "output"
 
 class bbox:
 	def __init__(self):
@@ -15,34 +19,54 @@ def threshold(filename):
 	binimg, contours, hierarchy = cv.findContours(threshold, 1, 2)
 	return binimg, contours
 
-def save(img, imgnum):
-	filename = "output/letter" + str(imgnum) + ".jpeg"
+def getInputFilename(filenameWithExt):
+	inputFilename, inputFileExt = filenameWithExt.split('.')
+	completeName = inputFolder + "/" + filenameWithExt 
+	return completeName, inputFilename, inputFileExt
+
+def getOutputFilename(imgNum, filename, ext):
+	return outputFolder + "/" + str(filename) + "/letter" + str(imgNum) + "." + str(ext)
+
+def save(img, imgNum, filename, ext):
+	filename = getOutputFilename(imgNum, filename, ext)
+	print(filename)
 	cv.imwrite(filename, img)
 
-def proccessimg(img, contour):
+def isValidShape(img, w):
+	# check if is not the contour of the entire img
+	return w < (img.shape[1]-10)
+
+def proccessImg(img, contour):
 	x,y,w,h = cv.boundingRect(contour)
-	if w < (img.shape[1]-10):
-	  	charimg = img[y:y+h, x:x+w]
-	  	return charimg
+	if (isValidShape(img, w)):
+	  	charImg = img[y:y+h, x:x+w]
+	  	return charImg
 	return None
 
-def get_letters(img, contours):
+def getLetters(img, contours):
 	letters = []
 	for contour in contours:
-		letterimg = proccessimg(img, contour)
+		letterimg = proccessImg(img, contour)
 		if letterimg is not None:
 			letters.append(letterimg)
 	return letters
 
-def save_letters(imgs):
+def createFolder(name):
+	directory = outputFolder + "/" + name
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+
+def saveLetters(imgs, inputFilename, ext):
 	size = len(imgs)
 	index = size
-	for letterimg in imgs:
-		save(letterimg, index)
+	createFolder(inputFilename)
+	for letterImg in imgs:
+		save(letterImg, index, inputFilename, ext)
 		index -= 1
 
 #def main():
 if __name__ == '__main__':
-	img, contours = threshold('input/sample.jpeg')
-	letters = get_letters(img, contours)
-	save_letters(letters)
+	path, filename, ext = getInputFilename("sample.jpeg")
+	img, contours = threshold(path)
+	letters = getLetters(img, contours)
+	saveLetters(letters, filename, ext)
